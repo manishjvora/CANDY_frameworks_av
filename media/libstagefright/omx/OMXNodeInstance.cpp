8722981c683515465ca12f7e94ace3cb8319e987
Robert Shih <robertshih@google.com>
2017-05-22 17:32:29 -0700

    DO NOT MERGE Don't leak `this` out of GraphicBufferSource ctor
    Bug: 37622974
    Bug: 37622987
    Bug: 37623757
    Test: run poc and observe no crash
    Change-Id: I1e25c011f02bec26a1480ec9a217a52f15d43cf2
    (cherry picked from commit 6301f882512ec39baf28640f31b90104def1738d)

--- media/libstagefright/omx/OMXNodeInstance.cpp
+++ media/libstagefright/omx/OMXNodeInstance.cpp
@@ -1135,7 +1135,7 @@ status_t OMXNodeInstance::createGraphicBufferSource(
             usageBits,
             bufferConsumer);
 
-    if ((err = bufferSource->initCheck()) != OK) {
+    if ((err = bufferSource->init()) != OK) {
         return err;
     }
     setGraphicBufferSource(bufferSource);
